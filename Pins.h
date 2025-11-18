23656691c15e2ad68e0d3c4c9f76723b4dbc9f84
enzosekito <103078089+enzosekito@users.noreply.github.com>
2025-11-18 14:52:00 +0000

    Update Pins.h

---
+++ Pins.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Servos pins
+#define PIN_SERVO_BASE   10
+#define PIN_SERVO_SHOULDER 9
+#define PIN_SERVO_ELBOW    6
+#define PIN_SERVO_WRIST    5
+
+//Sensors pins
+#define PIN_OBJ_SENSOR   A0  // sensor de objeto na zona de recolha
+#define PIN_DROP_SENSOR  A1  // sensor de objeto na zona de entrega
