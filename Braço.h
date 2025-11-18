4bf10ddf35bac06921ec2e83b69bc497cdef011e
enzosekito <103078089+enzosekito@users.noreply.github.com>
2025-11-18 15:42:10 +0000

    Update Braço.h

---
+++ Braço.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Arduino.h>
+#include <Servo.h>
+#include "Pins.h"
+#include "Config.h"
+
+// Guardar os servos
+typedef struct {
+  Servo base;
+  Servo shoulder;
+  Servo elbow;
+  Servo wrist;
+  int baseAngle;
+} arm_t;
+
+void arm_begin(arm_t &arm);
+
+// Posições pré-definidas (poses)
+void arm_pose_standby(arm_t &arm);
+void arm_pose_turn_pickup(arm_t &arm);
+void arm_pose_pickup_down(arm_t &arm);
+void arm_pose_close_claw(arm_t &arm);
+void arm_pose_carry_up(arm_t &arm);
+void arm_pose_turn_dropoff(arm_t &arm);
+void arm_pose_turn_dropoff(arm_t &arm);
+
+// Se precisares mexer só a base
+void arm_set_base(arm_t &arm, int deg);
