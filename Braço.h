688d038666b707cf2ae42395ad163415d2eb9ec8
enzosekito <103078089+enzosekito@users.noreply.github.com>
2025-11-18 15:47:55 +0000

    Update Braço.h

--- Braço.h
+++ Braço.h
@@ -22,7 +22,9 @@ void arm_pose_pickup_down(arm_t &arm);
 void arm_pose_close_claw(arm_t &arm);
 void arm_pose_carry_up(arm_t &arm);
 void arm_pose_turn_dropoff(arm_t &arm);
-void arm_pose_turn_dropoff(arm_t &arm);
+void arm_pose_carry_down(arm_t &arm);
+void arm_pose_open_claw(arm_t &arm);
 
 // Se precisares mexer só a base
 void arm_set_base(arm_t &arm, int deg);
+
