b9d396d50bd3c4753f6f2d41ea53c9dac483f073
enzosekito <103078089+enzosekito@users.noreply.github.com>
2025-11-25 14:21:17 +0000

    Update Pins.h

--- Pins.h
+++ Pins.h
@@ -7,5 +7,6 @@
 #define PIN_SERVO_WRIST    5
 
 //Sensors pins
-#define PIN_OBJ_SENSOR   A0  // sensor de objeto na zona de recolha
-#define PIN_DROP_SENSOR  A1  // sensor de objeto na zona de entrega
+#define PIN_SENSOR_PICKUP   A0  // sensor de objeto na zona de recolha
+#define PIN_SENSOR_DROPOFF  A1  // sensor de objeto na zona de entrega
+
