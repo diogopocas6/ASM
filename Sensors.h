89851805005ab4978d908d5e5dfd6e902cd841c8
enzosekito <103078089+enzosekito@users.noreply.github.com>
2025-11-25 14:24:06 +0000

    Update Sensors.h

--- Sensors.h
+++ Sensors.h
@@ -2,12 +2,15 @@
 #include <Arduino.h>
 #include "Pins.h"
 
+// Estrutura com as leituras de ambos os sensores
+typedef struct {
+  bool pieceAtPickup;   // há peça na zona de coleta?
+  bool pieceAtDropoff;  // há peça na zona de entrega?
+} sensors_t;
+
+// Inicializa os pinos dos sensores
 void sensors_begin();
 
-// devolve true se há peça na posição de recolha
-bool sensor_pieceDetected();
-
-// devolve true se o sensor da garra deteta objeto
-bool sensor_garraHasObject();
-
+// Atualiza a struct de leituras
+void sensors_read(sensors_t &s);
 
