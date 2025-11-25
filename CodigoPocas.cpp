393c9078b94f3243f06a4aa05f1d760d06a6ca42
diogopocas6 <81244186+diogopocas6@users.noreply.github.com>
2025-11-25 16:02:50 +0000

    Código Poças Ínicio

---
+++ CodigoPocas.cpp
@@ -0,0 +1,68 @@
+#include <Servo.h>
+#define PinBase 10
+#define PinShoulder 9
+#define PinElbow 6
+#define PinGarra 5 
+#define PinVentoinha 12
+#define PinSensorPickup A0
+#define PinSensorPlace A1
+#define PinSensorTemperatura A2 //Secalhar alterar estes números
+#define activateValueSensorPlace 100   //MUDAR CONFORME TESTES!
+#define activateValueSensorPick 100    //MUDAR CONFORME TESTES!
+
+Servo ServoBase;
+Servo ServoShoulder;
+Servo ServoElbow;
+Servo ServoGarra;
+
+typedef struct {
+  int value;       // leitura atual 
+  int prev_value;  // leitura anterior
+  bool re;          // rising edge: 0 -> 1, apenas acontece se ativo = 1 e prev_ativo = 0
+  bool ativo;       // se value > activateValueSensor, ativo = 1 else ativo = 0
+  bool prev_ativo;
+} Sensor;
+
+Sensor SensorPlace,SensorPickup;
+int temperatura = 0;
+int PWMventoinha = 0;
+void setup() {
+  pinMode(PinVentoinha,OUTPUT);
+  pinMode(PinSensorPickup,INPUT);       // VER SE É PRECISO POR INPUT_PULLUP
+  pinMode(PinSensorPlace,INPUT);        // VER SE É PRECISO POR INPUT_PULLUP
+  pinMode(PinSensorTemperatura,INPUT);  // VER SE É PRECISO POR INPUT_PULLUP
+  ServoBase.attach(PinBase);            //ServoBase.write(x) para mover para a posiçao x graus 
+  ServoShoulder.attach(PinShoulder);    //ServoShoulder.write(x) para mover para a posiçao x graus
+  ServoElbow.attach(PinElbow);
+  ServoGarra.attach(PinGarra);
+}
+
+void updateSensors(){
+// Sensor Temperatura
+  temperatura = analogRead(PinSensorTemperatura);
+
+  // SensorPlace
+  SensorPlace.prev_ativo = SensorPlace.ativo;
+  SensorPlace.prev_value = SensorPlace.value;
+  SensorPlace.value = analogRead(PinSensorPlace);
+  SensorPlace.ativo = (SensorPlace.value > activateValueSensorPlace);   // DEFINIR activateValueSensorPlace no define
+  SensorPlace.re = (!SensorPlace.prev_ativo && SensorPlace.ativo);  // re ativa quando passamos de não ativo para ativo
+
+  // SensorPickup
+  SensorPickup.prev_ativo = SensorPickup.ativo;
+  SensorPickup.prev_value = SensorPickup.value;
+  SensorPickup.value = analogRead(PinSensorPickup);
+  SensorPickup.ativo = (SensorPickup.value > activateValueSensorPick);   // DEFINIR activateValueSensorPick no define
+  SensorPickup.re = (!SensorPickup.prev_ativo && SensorPickup.ativo); // re ativa quando passamos de não ativo para ativo
+}
+
+void controlaVentoinha(int temperatura){
+  PWMventoinha = map(temperatura, 0, 1023, 0, 255);
+  analogWrite(PinVentoinha,PWMventoinha);
+}
+
+void loop() {
+  updateSensors();
+  controlaVentoinha(temperatura);
+
+}
